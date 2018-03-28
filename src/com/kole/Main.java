efafdece5ef2afdd8946aa3d78b8ee51643f2752
Slobodan <slobodan.kosanovic@gmail.com>
2018-03-28 17:28:13 +0200

    Initial commit

---
+++ src/com/kole/Main.java
@@ -0,0 +1,15 @@
+package com.kole;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+    }
+}
