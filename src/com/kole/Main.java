1a57d3239ae5b6e37dcee96b8665ce70e11b4132
Slobodan <slobodan.kosanovic@gmail.com>
2018-03-28 17:47:51 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -11,5 +11,24 @@ public class Main {
         // double
         // char
         // boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is " + myString);
+        myString = myString + ", and this is more";
+        System.out.println(myString);
+        myString = myString + "\u00A9 2015";
+        System.out.println(myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println(lastString);
+        double doubleNumber = 120.47;
+        lastString = lastString + doubleNumber;
+        System.out.println(lastString);
     }
 }
